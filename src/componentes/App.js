1c636d9b9b771da962487233377e3beb9f231a26
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-06 12:38:51 +0100

    Agregar componente ContenidoApp y actualizar título en index.html

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../estilos/App.scss';
 import Encabezado from './encabezado/encabezado';
 import Entradas from './encabezado/entradas';
+import ContenidoApp from './contenido/contenidoApp';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <div>
         <Encabezado />
         <Entradas />
+        <ContenidoApp />
       </div>
     );
   }
