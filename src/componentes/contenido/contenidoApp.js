1677953bd35ecd4db58187737cd93bbd49063cfc
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-06 12:38:51 +0100

    Agregar componente ContenidoApp y actualizar título en index.html

---
+++ src/componentes/contenido/contenidoApp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../../estilos/contenido/contenidoApp.scss';
+
+export default class ContenidoApp extends Component {
+  render() {
+    return (
+      <div className='contenido_app_container'>
+        <div className='contenido_app_titulo'>
+          Descubre la mejor aplicación de música
+        </div>
+        <div className='contenido_app_descripcion'>
+          Escucha tus canciones favoritas en cualquier lugar, en cualquier momento.
+        </div>
+        <div className='contenido_app_boton'>
+          Descargar
+        </div>
+      </div>
+
+    );
+  }
+}
