cee00c40ff6c7284a8576a5084a587cc1537c398
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-07 19:25:31 +0100

    Agregar imágenes de logo y fecha en ContenidoApp; actualizar estilos SASS

--- src/componentes/contenido/contenidoApp.js
+++ src/componentes/contenido/contenidoApp.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import '../../estilos/contenido/contenidoApp.scss';
+import Logo from '../../assets/images/LogoKoba.png';
+import Fecha from '../../assets/images/fecha.jpg';
+
 
 export default class ContenidoApp extends Component {
   render() {
     return (
       <div className='contenido_app_container'>
-        <div className='contenido_app_titulo'>
-          Descubre la mejor aplicación de música
-        </div>
-        <div className='contenido_app_descripcion'>
-          Escucha tus canciones favoritas en cualquier lugar, en cualquier momento.
-        </div>
-        <div className='contenido_app_boton'>
-          Descargar
-        </div>
+        <img src={Fecha} alt="Logo Koba" className="fecha_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
+        <img src={Logo} alt="Logo Koba" className="logo_img" />
       </div>
     );
   }
