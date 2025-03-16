ca24a302cbb3f3cb991558a5cd856c2c8d40fe5f
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-16 19:06:11 +0100

    Agregar nuevos componentes y estilos: crear FAQ y Sobre; eliminar contenido obsoleto y actualizar rutas de navegación.

--- src/componentes/pagina/Home.js
+++ src/componentes/pagina/Home.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Encabezado from '../encabezadoPie/encabezado';
-import ContenidoHome from '../contenido/contenidoHome';
-import Pie from '../encabezadoPie/pie';
+import ContenidoHome from '../contenido/homeContenido';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Encabezado />
         <ContenidoHome />
-        <Pie />
       </div>
     );
   }
