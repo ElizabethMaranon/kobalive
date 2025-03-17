c5f4dd02cd34f7ec13687ee8713e4251e55c72e2
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-17 20:47:29 +0100

    Refactor estructura de componentes y mejorar contenido de la sección FAQ

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -7,7 +7,7 @@ import Pie from './encabezadoPie/pie';
 // Páginas de la aplicación
 import Home from './pagina/home';
 import Sobre from './pagina/sobre';
-import Faq from './contenido/faqContenido';
+import Faq from './pagina/faq';
 import Merchandising from './pagina/merchandising';
 
 export default class App extends Component {
@@ -25,8 +25,7 @@ export default class App extends Component {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
-            <Route path="/faq" element={<Faq />} 
-            />
+            <Route path="/faq" element={<Faq />} />
             <Route path="/merchandising" element={<Merchandising />} />
           </Routes>
         </Router>
