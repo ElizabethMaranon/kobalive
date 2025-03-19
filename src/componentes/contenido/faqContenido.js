e5066e0e013cbfa1b3e9d7238a81d069f86d2ec4
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-19 12:21:04 +0100

    Corregir errores tipográficos y mejorar la redacción en faqContenido.js

--- src/componentes/contenido/faqContenido.js
+++ src/componentes/contenido/faqContenido.js
@@ -7,11 +7,11 @@ export default class FaqContenido extends Component {
       <div className="faq">
         <h1>Preguntas frecuentes</h1>
         <h2>¿Donde se celebrará el festival KOBA Live?</h2>
-        <h3>El KOBA Live se celebrará en las instalaciones de las escuelas de Matiena (Abadiño). Aquí puedes ver la ubicación del festival. También puedes accceder a la ubicaión desde el menú de la web.</h3>
+        <h3>El KOBA Live se celebrará en las instalaciones de las escuelas de Matiena (Abadiño). Aquí puedes ver la ubicación del festival. También puedes acceder a la ubicación desde el menú de la web.</h3>
 
-<h2>¿Como puedo desplazarme hasta la localidad de celebración del KOBA Live?</h2>
+        <h2>¿Como puedo desplazarme hasta la localidad de celebración del KOBA Live?</h2>
         <h3>El KOBA Live se celebra en Matiena una zona de Abadiño que se encuentra muy bien comunicada con los diferentes pueblos de la región. Puedes desplazarte en tren, en autobus y por supuesto en tu propio coche. La parada del tren y la del autobus se encuentran muy cercanas al recinto del festival. En caso de que te desplaces en coche no tendrás problemas de aparcamiento. En las inmediaciones al recinto hay zonas de parking gratuito.</h3>
-        <h2>Tengo una minusvalia, ¿se puede acceder al recinto en silla de ruedas?.</h2>
+        <h2>Tengo una minusvalía, ¿se puede acceder al recinto en silla de ruedas?.</h2>
         <h3>Si, el acceso al recinto está preparado para poder entrar en silla de ruedas sin ningún problema.</h3>
         <h2>¿Donde puedo conseguir entradas para ver el festival?</h2>
         <h3>La primera (año 2022) y segunda (año 2023) edición del festival KOBA Live es totalmente gratis, no se necesita ningún tipo de entrada para poder acceder al recinto y disfrutar del festival.</h3>
@@ -22,7 +22,7 @@ export default class FaqContenido extends Component {
         </h3>
         <h2>Quiero colaborar. ¿Que puedo hacer?.
         </h2>
-        <h3>Fundamentalmente, acudiendo al festival. También puedes colaborar comprando algun artículo del merchandise que nos ayudará para continuar realizando futuras ediciones y haciendo que el festival sea cada vez más grande y más importante dentro de la escena metal local y nacional. Si quieres realizar alguna colaboración más importante o tienes una propuesta, puedes ponerte en contacto con nosotros en este e-mail: info@koba-live.com
+        <h3>Fundamentalmente, acudiendo al festival. También puedes colaborar comprando algún artículo del merchandise que nos ayudará para continuar realizando futuras ediciones y haciendo que el festival sea cada vez más grande y más importante dentro de la escena metal local y nacional. Si quieres realizar alguna colaboración más importante o tienes una propuesta, puedes ponerte en contacto con nosotros en este e-mail: info@koba-live.com
 
         </h3>
         <h2>Tengo una banda y quiero participar en el KOBA Live. ¿Que puedo hacer?.
