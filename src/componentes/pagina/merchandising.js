f23047942474d0ec59be53e8fabe4bd9ecb2e52f
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-26 18:19:39 +0100

    Updates

--- src/componentes/pagina/merchandising.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import MerchandisingContenido from '../contenido/merchandisingContenido';
-
-export default class Merchandising extends Component {
-render () {
-return ;
-}
-}
--- src/env.js
+++
@@ -1,7 +0,0 @@
-const env = {
-  API_URL: "https://api.kobalive.com",
-  NODE_ENV: process.env.NODE_ENV || "development",
-  // ...otras variables de entorno...
-};
-
-export default env;
