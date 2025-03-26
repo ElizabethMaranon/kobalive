1602faefd75bd0f39e2abbed4a295a4b6309c22a
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-26 18:13:40 +0100

    Actualizar nombre de ruta en App.js, agregar índice de imágenes y mejorar estilos en sobreContenido

---
+++ src/assets/imageIndex.js
@@ -0,0 +1,22 @@
+// Bandas
+import Suicidal from './images/Bandas/Suicidal_Angels_Logo.png';
+import Logo from './images/Koba/LogoKoba.png';
+import Equipo from './images/Koba/equipoKoba1024x768.jpg';
+import Perfil from './images/Koba/perfil_pua.jpg';
+
+const Bandas = {
+  Suicidal,
+};
+
+const Koba = {
+  Logo,
+  Equipo,
+  Perfil
+}
+
+const Images = {
+  Bandas,
+  Koba,
+}
+
+export default Images;
