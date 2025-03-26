32933a91be0b787cb10833d41930377bbc4bcca4
MielmaDev <elizabethmaranon80@gmail.com>
2025-03-26 18:33:46 +0100

    Agregar archivos index.html e index.js para la estructura inicial de la aplicación React

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../src/App'; // Asegúrate de que la ruta sea correcta
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
